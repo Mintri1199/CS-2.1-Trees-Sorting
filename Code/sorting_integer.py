67fd329a65a4166bcecfc6efc89f152c84951cec
Jackson Ho <mintri1199@gmail.com>
2019-12-09 17:51:16 -0800

    [Modified] Finished integer sorting

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -1,4 +1,70 @@
 #!python
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return repr(self.data)
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def __repr__(self):
+        """
+        Return a string representation of the list.
+        Takes O(n) time.
+        """
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(repr(curr))
+            curr = curr.next
+        return '[' + ', '.join(nodes) + ']'
+
+    def insert_asc(self, data):
+        new_node = Node(data)
+
+        # Case where the linked list is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.size += 1
+            return
+
+        # Case where the insert is prepend
+        if data < self.head.data:
+            new_node.next = self.head
+            self.head = new_node
+            self.size += 1
+            return
+
+        current_node = self.head
+
+        # Find the node
+        while current_node is not None:
+            if data == current_node.data:
+                break
+            elif current_node.next is not None and current_node.next.data > data:
+                break
+
+            current_node = current_node.next
+
+        # Case where the function reach the end of the linked list
+        if current_node is None:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.size += 1
+
+        else:
+            new_node.next = current_node.next
+            current_node.next = new_node
+            self.size += 1
 
 
 def counting_sort(numbers):
@@ -6,11 +72,27 @@ def counting_sort(numbers):
     then looping over counts and copying that many numbers into output list.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
-    # TODO: Loop over counts and append that many numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    min_num = numbers[0]
+    max_num = numbers[0]
+    # Find the range
+    for num in numbers:
+        if num < min_num:
+            min_num = num
+        elif num > max_num:
+            max_num = num
+
+    result_array = []
+    array_size = [0] * (max_num - min_num + 1)
+    for num in numbers:
+        array_size[num - min_num] += 1
+
+    offset = 0
+    for i, occ in enumerate(array_size):
+        if occ != 0:
+            # mutate the input
+            for j in range(occ):
+                numbers[j + offset] = i + min_num
+            offset += occ
 
 
 def bucket_sort(numbers, num_buckets=10):
@@ -18,9 +100,27 @@ def bucket_sort(numbers, num_buckets=10):
     then sorting each bucket and concatenating all buckets in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
-    # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    min_num = numbers[0]
+    max_num = numbers[0]
+    # Find the range
+    for num in numbers:
+        if num < min_num:
+            min_num = num
+        elif num > max_num:
+            max_num = num
+
+    buckets = [LinkedList() for x in range(num_buckets)]
+    divider = (max_num + 1) // num_buckets
+    for num in numbers:
+        bucket = buckets[num // divider]
+        bucket.insert_asc(num)
+
+    pointer = 0
+
+    for bucket in buckets:
+        current_node = bucket.head
+
+        while current_node is not None:
+            numbers[pointer] = current_node.data
+            current_node = current_node.next
+            pointer += 1
